3240148f81a227ab7c1b90eeb7d49afac6e388b0
Michel de Quadros Kruger <micheldequadros@hotmail.com>
2021-06-24 21:49:40 -0300

    create the context, and Home and New Room pages

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import './styles/global.scss';
 
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
+
+    return (
+        <AuthContextProvider>
+            <BrowserRouter>
+                <Route path="/" exact component={Home}/>
+                <Route path="/rooms/new" component={NewRoom}/> 
+            </BrowserRouter>
+        </AuthContextProvider>
+    );
 }
 
 export default App;
--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,5 +1,5 @@
-
-import { useHistory } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import illustrationImg from '../assets/images/illustration.svg';
 import LogoImg from '../assets/images/logo.svg'
@@ -7,29 +7,14 @@ import GoogleIcon from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss';
 import { Button } from "../components/Button";
-import { useAuth } from "../hooks/useAuth";
 
-export function Home() {
-
-    const history = useHistory();
-
-    const { user, signInWithGoogle } = useAuth()
-
-    async function handleCreateRoom() {
-
-        if(!user) {
-            await signInWithGoogle()
-        }
-
-        history.push('/rooms/new')
-    }
-
+export function NewRoom() {
     return(
        <div id="page-auth">
            <aside>
                 <img src={illustrationImg} alt="illustration pic"/>
                 <strong>
-                    Crie salas de Q&amp;A ao-vivo
+                    Crie salas de Q$amp;A ao-vivo
                 </strong>
                 <p>
                     Tire as dúvidas da sua audiência em tempo-real
@@ -38,21 +23,18 @@ export function Home() {
            <main>
                <div className="main-container">
                     <img src={LogoImg} alt="LetMeAsk"/>
-                    <button className="create-room" onClick={handleCreateRoom}>
-                            <img src={GoogleIcon} alt="Google Icon"/>
-                            Crie a sua Sala com o Google
-                    </button>
-                    <div className="separator">
-                        ou entre em uma sala
-                    </div>
+                    <h2> Criar uma nova sala </h2>
                     <form action="">
                         <input
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar na sala
+                            Criar sala
                         </Button>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+                    </p>
                     </form>
                </div>
            </main>
